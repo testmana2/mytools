f745a11a64271781bfc6da5aa4b527635471acea
testmana2 <git@localhost>
2016-01-03 12:35:01 +0800

    modify settings

--- shadowscrapy/shadowscrapy/settings.py
+++ shadowscrapy/shadowscrapy/settings.py
@@ -14,4 +14,6 @@ SPIDER_MODULES = ['shadowscrapy.spiders']
 NEWSPIDER_MODULE = 'shadowscrapy.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'shadowscrapy (+http://www.yourdomain.com)'
+USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1"
+
+COOKIES_ENABLES = False
